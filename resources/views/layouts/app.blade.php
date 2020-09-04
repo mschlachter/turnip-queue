7c50ea36156664f947dbac8f72b634729ab7a89d
Matthew Schlachter <matthew@webmann.info>
2020-09-04 07:04:21 -0400

    Move search engine logic to meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    @stack('meta')
+
     <title>{{ config('app.name', 'Turnip Queue') }}</title>
 
     <!-- Fonts -->
