da64f7d8ab596282dcc00c071585c029980ba457
Matthew Schlachter <matthew@webmann.info>
2020-09-04 12:01:44 -0400

    Add npm-install-if-needed script

---
+++ npm-install-if-changed.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+
+checkForChangedFiles() {
+    echo "$changedFiles" | grep --quiet "$1" && eval "$2"
+}
+
+packageJsonHasChanged() {
+  echo "Changes to package.json detected, installing updates"
+  npm i
+}
+
+checkForChangedFiles package.json packageJsonHasChanged
