e3ca8c3da410ef03fdfd46ad623173fa7c42f0e8
Matthew Schlachter <matthew@webmann.info>
2020-09-04 07:37:41 -0400

    Fix link to purge queues command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\PurgeAbandonnedSeekers;
+use App\Console\Commands\PurgeExpiredQueues;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         PurgeAbandonnedSeekers::class,
+        PurgeExpiredQueues::class,
     ];
 
     /**
