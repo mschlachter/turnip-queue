c72bcea44da4ddd0cff5b9b07e6dcf9cb42941b0
Matthew Schlachter <matthew@webmann.info>
2020-09-04 23:56:43 -0400

    Remove unused method

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -24,14 +24,4 @@ class BroadcastServiceProvider extends ServiceProvider
 
         require base_path('routes/channels.php');
     }
-
-    private function ends_with($haystack,$needle,$case=true)
-    {
-        $expectedPosition = strlen($haystack) - strlen($needle);
-
-        if ($case)
-            return strrpos($haystack, $needle, 0) === $expectedPosition;
-
-        return strripos($haystack, $needle, 0) === $expectedPosition;
-    }
 }
